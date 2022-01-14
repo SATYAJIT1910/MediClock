b986823e10e72949becbd598496cd94b08e9f574
Satyajit Ghosh <satyajit.edu@outlook.com>
2022-01-14 19:45:21 +0530

    google signin implemented on appside

---
+++ app/src/main/java/com/satyajitghosh/mediclock/HomeActivity.java
@@ -0,0 +1,25 @@
+package com.satyajitghosh.mediclock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+
+public class HomeActivity extends AppCompatActivity {
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("UserName");
+        textView=findViewById(R.id.textView);
+        textView.setText(name+" Logged In");
+    }
+}
