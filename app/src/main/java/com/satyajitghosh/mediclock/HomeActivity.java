fa1477172c75d0f01f3a84b85b21784c327252c3
Satyajit Ghosh <satyajit.edu@outlook.com>
2022-01-14 20:31:33 +0530

    implemented the logout
    added logout feature

--- app/src/main/java/com/satyajitghosh/mediclock/HomeActivity.java
+++ app/src/main/java/com/satyajitghosh/mediclock/HomeActivity.java
@@ -6,20 +6,44 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
 public class HomeActivity extends AppCompatActivity {
     private TextView textView;
+    private Button logout;
+    private GoogleSignInClient mGoogleSignInClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
         Intent intent = getIntent();
         String name = intent.getStringExtra("UserName");
         textView=findViewById(R.id.textView);
         textView.setText(name+" Logged In");
+        logout=findViewById(R.id.button);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mGoogleSignInClient.signOut();
+                Toast.makeText(getApplicationContext(),"Logged Out",Toast.LENGTH_LONG).show();
+                startActivity(new Intent(HomeActivity.this,MainActivity.class));
+            }
+        });
+
     }
+
 }
