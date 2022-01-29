4fff7c57c51fc5b1d0bde6f3112520862eecd4c5
Satyajit Ghosh <satyajit.edu@outlook.com>
2022-01-29 19:48:19 +0530

    bottom app bar and ui improvemented added

--- app/src/main/java/com/satyajitghosh/mediclock/DisplayMedicineActivity.java
+++ app/src/main/java/com/satyajitghosh/mediclock/DisplayMedicineActivity.java
@@ -2,6 +2,7 @@ package com.satyajitghosh.mediclock;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.widget.ListView;
@@ -10,9 +11,11 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -35,6 +38,7 @@ public class DisplayMedicineActivity extends AppCompatActivity {
     private ArrayList<MedicineRecordHandler> arrayList;
     protected GoogleSignInAccount account;
     private CustomAdapter c;
+    private BottomAppBar bottomAppBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +48,7 @@ public class DisplayMedicineActivity extends AppCompatActivity {
         account = GoogleSignIn.getLastSignedInAccount(this);
         mDatabase = FirebaseDatabase.getInstance().getReference().child("MedicineRecord").child(Objects.requireNonNull(account.getId())); //Takes the relative path of the user to get the instance of only that user not others.
         listview = findViewById(R.id.listview);
+        bottomAppBar = findViewById(R.id.bottomAppBar);
 
         arrayList = new ArrayList<>();
         TextView account_user_name_view = findViewById(R.id.account_user_name_view);
@@ -78,13 +83,26 @@ public class DisplayMedicineActivity extends AppCompatActivity {
                 );
             }
         });
-        findViewById(R.id.setting_btn).setOnClickListener(new View.OnClickListener() {
+        bottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), TimeChangeActivity.class));
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_settings:
+                        startActivity(new Intent(getApplicationContext(), TimeChangeActivity.class));
+                        break;
+                    case R.id.nav_doctor:
+                        break;
+                    case R.id.nav_lab:
+                        break;
+                    default:
+                        return false;
+                }
+                return true;
             }
         });
     }
+
+
 
     /**
      * This method is used to update the arraylist on CustomAdapter and refreshes the listview
