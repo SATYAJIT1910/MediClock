7029cb97c88c951d11e160cd5b0e03e37c19d282
Satyajit Ghosh <satyajit.edu@outlook.com>
2022-01-30 13:48:10 +0530

    ui added on appointment  add acivity

--- app/src/main/java/com/satyajitghosh/mediclock/doctor/DoctorAddActivity.java
+++ app/src/main/java/com/satyajitghosh/mediclock/doctor/DoctorAddActivity.java
@@ -1,31 +1,61 @@
 package com.satyajitghosh.mediclock.doctor;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 import com.google.android.material.timepicker.MaterialTimePicker;
 import com.satyajitghosh.mediclock.R;
 
+import java.util.Calendar;
+
 public class DoctorAddActivity extends AppCompatActivity {
+    public static String date;
+    public static TextView date_view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_add);
-        findViewById(R.id.date_view).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                MaterialDatePicker datePicker =
-                        MaterialDatePicker.Builder.datePicker()
-                                .setTitleText("Select date")
-                                .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
-                                .build();
-
-                datePicker.show(getSupportFragmentManager(), "tag");
-
-            }
-        });
+        date_view=findViewById(R.id.date_view);
     }
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+public static class DatePickerFragment extends DialogFragment
+        implements DatePickerDialog.OnDateSetListener {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current date as the default date in the picker
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        // Create a new instance of DatePickerDialog and return it
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        // Do something with the date chosen by the user
+        String result=Integer.toString(day)+"/"+Integer.toString(month+1)+"/"+Integer.toString(year);
+        date=result;
+        date_view.setText(date);
+        Toast.makeText(getContext(),result,Toast.LENGTH_LONG).show();
+    }
+
+}
 }
